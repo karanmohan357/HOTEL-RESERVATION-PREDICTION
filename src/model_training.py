168bfffe41d08bfc9ee1b81d7c73d95ad1a7c3a7
Karan <karanmohan357@gmail.com>
2026-09-01 04:22:36 +0530

    Update MLflow tracking URI to use SQLite database for experiment logging

--- src/model_training.py
+++ src/model_training.py
@@ -109,7 +109,7 @@ class ModelTraining:
       
     def run(self):
         try:
-            mlflow.set_tracking_uri("file:./mlruns")
+            mlflow.set_tracking_uri("sqlite:///mlflow.db")
             mlflow.set_experiment("Hotel_Booking_Cancellation_Prediction")
             with mlflow.start_run():
 
